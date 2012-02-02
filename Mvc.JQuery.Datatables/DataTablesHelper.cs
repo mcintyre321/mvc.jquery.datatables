a148cef2c3c706e0ae310bad4d81b5d432c5a828
mcintyre321 <mcintyre321@gmail.com>
2012-02-02 19:33:25 +0000

    Added helper to render scripts

--- Mvc.JQuery.Datatables/DataTablesHelper.cs
+++ Mvc.JQuery.Datatables/DataTablesHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
 
@@ -9,13 +10,22 @@ namespace Mvc.JQuery.Datatables
 {
     public static class DataTablesHelper
     {
-        
+        public static IHtmlString DataTableIncludes(this HtmlHelper helper, bool css = true, bool filters = true)
+        {
+            const string cssHtml = @"<link type=""text/css"" href=""@Url.Content(""~/Content/DataTables-1.8.2/media/css/demo_table.css"" rel=""stylesheet""/>";
+
+            const string jsHtml = @"<script src=""/Scripts/DataTables-1.8.2/media/js/jquery.dataTables.js"" type=""text/javascript""></script>";
+            const string filtersHtml = @"<script src=""/Scripts/DataTables-1.8.2/extras/columnfilter/jquery.dataTables.columnFilter.js"" type=""text/javascript""></script>";
+            return helper.Raw(jsHtml + (filters ? filtersHtml : "") + (css ? cssHtml : ""));
+
+        }
+
         public static MvcHtmlString DataTable<TController, TResult>(this HtmlHelper html, string id, Expression<Func<TController, DataTablesResult<TResult>>> exp, params string[] columns)
         {
-            if (columns == null || columns.Length == 0) columns = typeof (TResult).GetProperties().Where(p => p.GetGetMethod() != null).Select(p => p.Name).ToArray();
+            if (columns == null || columns.Length == 0) columns = typeof(TResult).GetProperties().Where(p => p.GetGetMethod() != null).Select(p => p.Name).ToArray();
 
             var mi = exp.MethodInfo();
-             var controllerName = typeof(TController).Name;
+            var controllerName = typeof(TController).Name;
             controllerName = controllerName.Substring(0, controllerName.LastIndexOf("Controller"));
             var urlHelper = new UrlHelper(html.ViewContext.RequestContext);
             var ajaxUrl = urlHelper.Action(mi.Name, controllerName);

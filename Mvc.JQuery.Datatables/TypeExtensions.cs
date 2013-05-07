d71e2267c2adc5bb59af3ac7c14b2325fe9d92fa
AthosXtreme <AthosXtreme@gmail.com>
2013-05-07 13:40:18 -0400

    Add namespace and fix DisplayAttribute.Order evaluation to avoid exception when Order is not set.

--- Mvc.JQuery.Datatables/TypeExtensions.cs
+++ Mvc.JQuery.Datatables/TypeExtensions.cs
@@ -4,29 +4,32 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
 
-public static class TypeExtensions
+namespace Mvc.JQuery.Datatables
 {
-    public static IEnumerable<PropertyInfo> GetSortedProperties(this Type t)
+    public static class TypeExtensions
     {
-        return from pi in t.GetProperties()
-               let da = (DisplayAttribute)pi.GetCustomAttributes(typeof(DisplayAttribute), false).SingleOrDefault()
-               let order = ((da != null && da.Order != 0) ? da.Order : int.MaxValue)
-               orderby order
-               select pi;
-    }
+        public static IEnumerable<PropertyInfo> GetSortedProperties(this Type t)
+        {
+            return from pi in t.GetProperties()
+                   let da = (DisplayAttribute)pi.GetCustomAttributes(typeof(DisplayAttribute), false).SingleOrDefault()
+                   let order = ((da != null && da.GetOrder() != null && da.GetOrder() >= 0) ? da.Order : int.MaxValue)
+                   orderby order
+                   select pi;
+        }
 
-    public static IEnumerable<PropertyInfo> GetSortedProperties<T>()
-    {
-        return typeof(T).GetSortedProperties();
-    }
-    public static IEnumerable<PropertyInfo> GetProperties(this Type t)
-    {
-        return from pi in t.GetProperties()
-               select pi;
-    }
+        public static IEnumerable<PropertyInfo> GetSortedProperties<T>()
+        {
+            return typeof(T).GetSortedProperties();
+        }
+        public static IEnumerable<PropertyInfo> GetProperties(this Type t)
+        {
+            return from pi in t.GetProperties()
+                   select pi;
+        }
 
-    public static IEnumerable<PropertyInfo> GetProperties<T>()
-    {
-        return typeof(T).GetSortedProperties();
+        public static IEnumerable<PropertyInfo> GetProperties<T>()
+        {
+            return typeof(T).GetSortedProperties();
+        }
     }
 }

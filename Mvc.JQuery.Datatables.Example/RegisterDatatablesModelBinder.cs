64b4dafd26b0eb6116bb6f3904d530653d94962d
Harry McIntyre <mcintyre321@gmail.com>
2014-04-13 14:20:53 +0100

    Add model binder to example project

---
+++ Mvc.JQuery.Datatables.Example/RegisterDatatablesModelBinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc.JQuery.Datatables.Example;
+
+[assembly: PreApplicationStartMethod(typeof(RegisterDatatablesModelBinder), "Start")]
+
+namespace Mvc.JQuery.Datatables.Example
+{
+    public static class RegisterDatatablesModelBinder
+    {
+        public static void Start()
+        {
+            if (!ModelBinders.Binders.ContainsKey(typeof(DataTablesParam)))
+                ModelBinders.Binders.Add(typeof(DataTablesParam), new Mvc.JQuery.Datatables.DataTablesModelBinder());
+        }
+    }
+}

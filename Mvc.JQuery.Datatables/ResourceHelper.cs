3935968e02eda88c359d79538010f0f4c6d5d5eb
Dragan Mihajlović <draza67@hotmail.com>
2015-07-01 01:20:29 +0200

    Update ResourceHelper.cs

--- Mvc.JQuery.Datatables/ResourceHelper.cs
+++ Mvc.JQuery.Datatables/ResourceHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mvc.JQuery.Datatables
 {
